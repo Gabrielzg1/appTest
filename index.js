d795d29dbba4c00bb08eeea9ceca0f8ea2fcd5fe
Gabriel Zanandre Guadagnini <gabrielzg173@gmail.com>
2022-03-23 22:10:56 -0300

    .

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const app = express()
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+  app.use(express.static(path.join(__dirname, 'public')))
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'ejs')
+  app.get('/', (req, res) => res.render('pages/index'))
+  app.get('/login', (req, res) => {
+    res.render('pages/login')
+  });
+  app.get('/about', (req, res) => res.render('pages/about'))
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
